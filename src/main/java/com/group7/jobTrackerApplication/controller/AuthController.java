f2c28a31cfe450b4275c4baea4bad0498d34acf3
Brandon Hernandez-Cano <brandon.z.cano@gmail.com>
2026-03-25 20:29:17 -0700

    Cleanup (#56)
    * Update Dockerfile
    
    * Update application.properties
    
    * Update SecurityConfig.java
    
    * Update application.properties
    
    * Remove unnecessary get all
    
    * Added tests and fixed old method naming to current method names
    
    * Added new test files for E2E and fixed security config to work with both localhost frontend and deployed frontend
    
    * api/me returns more user information for profile now

--- src/main/java/com/group7/jobTrackerApplication/controller/AuthController.java
+++ src/main/java/com/group7/jobTrackerApplication/controller/AuthController.java
@@ -7,8 +7,10 @@ import java.util.stream.Collectors;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 public class AuthController {
@@ -24,9 +26,12 @@ public class AuthController {
         User dbUser = userService.getOrCreateFromOAuth(user);
 
         return Map.of(
+                "userId", dbUser.getUserId(),
+                "username", dbUser.getUsername(),
                 "name", user.getAttribute("name"),
                 "login", user.getAttribute("login"),
                 "email", user.getAttribute("email"),
+                "oauthProvider", dbUser.getOauthProvider(),
                 "role", dbUser.getRole().name(),
                 "authorities", user.getAuthorities().stream()
                         .map(grantedAuthority -> grantedAuthority.getAuthority())
@@ -34,5 +39,12 @@ public class AuthController {
                         .collect(Collectors.toList()),
                 "attributes", user.getAttributes()
         );
+    }
+
+    @DeleteMapping("/api/me")
+    public ResponseEntity<Void> deleteCurrentUser(@AuthenticationPrincipal OAuth2User user) {
+        User dbUser = userService.getOrCreateFromOAuth(user);
+        userService.delete(dbUser.getUserId());
+        return ResponseEntity.noContent().build();
     }
 }
